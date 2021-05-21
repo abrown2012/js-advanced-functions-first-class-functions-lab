7ab756dc44cb1197f1ee480ed9424438b44c3731
Agnieszka Brown <abrown2012@icloud.com>
2021-05-20 21:44:21 -0400

    commit tree 35d731309a03fcfb942ec6723e14b520dfa65e48

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const  createFareMultiplier = (value) => {
+    return function(multiplier){
+        return value * multiplier
+    }
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2
+}
+
+const fareTripler = (fare) => {
+    return fare * 3
+}
+
+const selectDifferentDrivers = (arrayOfDrivers, fun) =>{
+    return fun(arrayOfDrivers)
+}
